d05ed9d191024fa666a6c927afddbd276ce5a7f7
BrianMureithiW <brianmureithi9@gmail.com>
2022-04-07 19:09:43 -0700

    final task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,12 +6,6 @@
  */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
-
 printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(b));
