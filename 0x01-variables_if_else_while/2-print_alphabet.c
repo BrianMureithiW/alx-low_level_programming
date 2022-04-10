2f07e25fb01f1be318fe0b7f8360b78c72f739ed
BrianMureithiW <brianmureithi9@gmail.com>
2022-04-10 13:41:25 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,15 +10,15 @@
 
 int main(void)
 {
-/*declaration*/	
+/*declaration*/
 
 	char letter;
 
-	for(letter ='a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
 	}
 	putchar('\n');
 
-	return 0;
+	return (0);
 }
