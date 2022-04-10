3190e8eb62f40d3b3bc0f373fac9d48874675b60
BrianMureithiW <brianmureithi9@gmail.com>
2022-04-10 15:35:35 -0700

    added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints digit
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+/*declaration*/
+
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
