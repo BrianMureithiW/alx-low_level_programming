d577fecf3cf50d2f036918f074c3b5ac2b1a65ef
BrianMureithiW <brianmureithi9@gmail.com>
2022-04-11 05:53:25 -0700

    added file 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+/*declaration*/
+
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 98; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 <= 99; digit2++)
+		{
+			putchar((digit1 / 10) + '0');
+			putchar((digit1 % 10) + '0');
+			putchar(' ');
+			putchar((digit2 / 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 98 && digit2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
