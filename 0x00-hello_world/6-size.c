222bea7b9b496dc69224aca5c92fc151bb667192
BrianMureithiW <brianmureithi9@gmail.com>
2022-04-11 06:34:11 -0700

    made corrections on task 6 of hello_world on c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,12 @@
  */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
 
-printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long long int: %d byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a float: %d byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("size of a float: %d byte(s)\n", sizeof(float));
+
 return (0);
 }
