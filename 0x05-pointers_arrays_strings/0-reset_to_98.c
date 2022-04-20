7f0dc1a0ad9511e440e001bfaccac7409873eeee
BrianMureithiW <brianmureithi9@gmail.com>
2022-04-20 13:10:19 -0700

    update betty doc

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - function that takes a pointer to an int as parameter
+ * reset_to_98 - function that takes a pointer to an int as parameter
  *
  * Return: always 0
  *
