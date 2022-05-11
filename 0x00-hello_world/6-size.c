12cc0636f0372bbd3a4b8fd0c36ff0b2661bbb0c
BrianMureithiW <brianmureithi9@gmail.com>
2022-05-11 11:45:31 -0700

    modifications

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <conio.h>
 
 /**
  * main - Entry point
@@ -14,5 +15,6 @@ int main(void)
 	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("size of a float: %d byte(s)\n", sizeof(float));
 
+	getch();
 	return (0);
 }
